6c39063ffa4f718329beb07bcab970295d2fd666
PURPLE <9707.hyeon@gmail.com>
2021-03-23 15:53:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI,Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# static management
+app.mount("/static", StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+
 @app.get("/")
 async def main(request:Request):
     return 0
