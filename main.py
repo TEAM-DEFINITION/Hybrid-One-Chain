fc9c0ffa9630a689e7e2b32cf6423db42c4283f8
PURPLE <9707.hyeon@gmail.com>
2021-03-23 20:30:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
-
+# 메인
 @app.get("/")
 async def main(request:Request):
     return 0
 
 
-# 회원 가입
+# 회원가입 모듈
 @app.get("/signup")
 async def signup(request:Request):
     return 0
@@ -25,5 +25,9 @@ async def signup(request:Request):
 async def auth(request:Request):
     return 0
 
+# 체인 검증 모듈
+@app.get("/validate")
+async def validate(request:Request):
+    return 0
 
 # 자동 시작
