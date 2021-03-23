b0cbb3095ac02dce08ba603cd51a498dbf34898a
PURPLE <9707.hyeon@gmail.com>
2021-03-23 12:36:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ app = FastAPI()
 @app.get("/")
 async def main(request:Request):
     return 0
+
+
+# 회원 가입
+@app.get("/signup")
+async def signup(request:Request):
+    return 0
+
+
+# 인증 모듈
+@app.get("/auth")
+async def auth(request:Request):
+    return 0
