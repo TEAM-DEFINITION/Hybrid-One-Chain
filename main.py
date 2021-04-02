197270bd4ef865481adfd997d00b7f285cccb9bb
PURPLE <9707.hyeon@gmail.com>
2021-04-03 03:15:37 +0900

    Update

--- main.py
+++ main.py
@@ -29,11 +29,6 @@ class RES(BaseModel):
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:*",
-    "http://localhost:55555",
     "*",
 ]
 
