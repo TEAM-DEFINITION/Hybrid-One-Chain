e848d4be07769f98dfa450e30116c795001acaac
PURPLE <9707.hyeon@gmail.com>
2021-04-12 00:43:21 +0900

    Patch find postcode fail

--- module_postcode.py
+++ module_postcode.py
@@ -1,15 +1,14 @@
 def check(postcode):
     
-    print("사용자가 방문할 포스트 코드 : " + postcode)
-    
     # 기존 가맹점 목록 불러오기
     f = open("db_store\\postcode","r", encoding="UTF8")
     prev_block = f.readlines()
     f.close()
 
+    # 포스트코드 찾기
     for i in prev_block:
         if i.find(postcode) == -1:
             pass
         else:
             return i.split("|")[1]
-    return "00000"
+    return "NULL"
